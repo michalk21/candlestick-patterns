8b44f405491b9f128a5e42a9c363893c2f93a12a
michalk21 <michalk21.mk@gmail.com>
2019-11-03 16:06:15 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="candlestick-patterns",
+    name="candlestick",
     version="0.0.1",
     author_email="DataSmoother",
     description="candlestick-patterns",
