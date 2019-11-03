c50ccb22d70fb2fa294260d6f7b41e49cc2c96a1
michalk21 <michalk21.mk@gmail.com>
2019-11-03 15:55:34 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="candlestick-patterns",
+    version="0.0.1",
+    author_email="DataSmoother",
+    description="candlestick-patterns",
+    py_modules = ['candlestick']
+)
